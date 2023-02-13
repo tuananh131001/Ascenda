0ef823b22889b2062b2360fb388b6a1ff10c99c1
William <s3864077@rmit.edu.vn>
2023-02-13 15:13:16 +0700

    Add Helloworld testing

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3000;
 
 app.use(express.json());
 app.use("/api", offerRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
